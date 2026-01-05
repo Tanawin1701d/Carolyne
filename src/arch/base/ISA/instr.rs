261c5b0c456203e51c9ce44bdd4e39d43035b1c7
tanawin <tanawin1701d@outlook.com>
2026-01-05 09:28:33 +0700

    refactor ISA module to introduce Operand and Uop interfaces for improved abstraction

--- src/arch/base/ISA/instr.rs
+++ src/arch/base/ISA/instr.rs
@@ -1,58 +1,80 @@
 use crate::arch::base::arf::arf::InstrWidth;
 
-pub struct IsaOperand<T: InstrWidth>{
-    valid    : bool,
-    index    : usize,
-    arfIndex : usize,
-    data     : T
+#[derive(Default, Copy, Clone)]
+
+// pub struct IsaOperand<T: InstrWidth>{
+//     index    : usize,
+//     arfIndex : usize,
+//     data     : T
+// }
+//
+// #[derive(Default, Copy, Clone)]
+// pub struct ISAUop<S0T: InstrWidth, S1T: InstrWidth,
+//                   D0T: InstrWidth, D1T: InstrWidth>{
+//     ///////// meta-data
+//     id   : u64,
+//     mop_id: u64,
+//     opcode: u32,
+//     ///////// operand
+//     s0: IsaOperand<S0T>,
+//     s1: IsaOperand<S1T>,
+//     d0: IsaOperand<D0T>,
+//     d1: IsaOperand<D1T>,
+//
+// }
+//
+// #[derive(Default, Copy, Clone)]
+// pub struct ISAMop<const MAX_AMT_MICRO_OPS: usize,
+//                       S0T: InstrWidth, S1T: InstrWidth,
+//                       D0T: InstrWidth, D1T: InstrWidth>
+// where
+//     ISAUop<S0T, S1T, D0T, D1T>: Default + Copy,
+// {
+//     id       : u64,
+//     pc       : u64,
+//     amt_uop  : u8,
+//     uops: [ISAUop<S0T, S1T, D0T, D1T>; MAX_AMT_MICRO_OPS]
+//
+// }
+//
+// impl<const N: usize, S0T, S1T, D0T, D1T> Default
+// for ISAMop<N, S0T, S1T, D0T, D1T>
+// where
+//     S0T: InstrWidth,
+//     S1T: InstrWidth,
+//     D0T: InstrWidth,
+//     D1T: InstrWidth,
+//     ISAUop<S0T, S1T, D0T, D1T>: Default + Copy,
+// {
+//     fn default() -> Self {
+//         Self {
+//             id: 0,
+//             pc: 0,
+//             amt_uop: 0,
+//             uops: std::array::from_fn(|_| ISAUop::default()),
+//         }
+//     }
+// }
+
+pub trait OperandItf {
+    fn is_valid(&self) -> bool;
+    fn get_index(&self) -> usize;
+    fn get_arf_index(&self) -> usize;
 }
 
-pub struct ISAUop<S0T: InstrWidth, S1T: InstrWidth,
-                      D0T: InstrWidth, D1T: InstrWidth>{
-    ///////// meta-data
-    id   : u64,
-    MopId: u64,
-    opcode: u32, 
-    ///////// operand
-    s0: IsaOperand<S0T>,
-    s1: IsaOperand<S1T>,
-    d0: IsaOperand<D0T>,
-    d1: IsaOperand<D1T>,
-    
-    
-    
+pub trait UopItf{
+
+    type Operand: OperandItf; /// it have to inherit OperandItf not only OperandItf
+
+    fn get_amt_src_opr(&self) -> usize;
+    fn get_src_opr_itf(&self, index: usize) -> &mut Self::Operand;
+
+    fn get_amt_dst_opr(&self) -> usize;
+    fn get_dst_opr_itf(&self, index: usize) -> &mut Self::Operand;
+
+    fn get_id(&self) -> u64;
 
 }
-
-pub struct ISAMop<const MAX_AMT_MICRO_OPS: usize,
-                      S0T: InstrWidth, S1T: InstrWidth,
-                      D0T: InstrWidth, D1T: InstrWidth>{
-    id       : u64,
-    pc       : u64,
-    amt_uop  : u8,
-    uops: [ISAUop<S0T, S1T, D0T, D1T>; MAX_AMT_MICRO_OPS]
-    
-}
-
-impl<const MAX_AMT_MICRO_OPS: usize, S0T, S1T, D0T, D1T> ISAMop<MAX_AMT_MICRO_OPS, S0T, S1T, D0T, D1T>
-where
-    S0T: InstrWidth,
-    S1T: InstrWidth,
-    D0T: InstrWidth,
-    D1T: InstrWidth,
-{
-    pub fn new(id: u64, pc: u64) -> Self {
-        Self {
-            id,
-            pc,
-            amt_uop: 0,
-            // Initializes the array using the Default implementation of ISAUop
-            uops: [ISAUop::default(); MAX_AMT_MICRO_OPS],
-        }
-    }
-}
-
-
 
 pub trait ISAOperable{
     fn execute_next(&self, id :u64) -> u64;
